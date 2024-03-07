676e7aad81660f98870dbce4d3eebd17eefa74ec
MireyaCastro70 <irma.castro2270@uppuebla.edu.mx>
2024-03-07 13:00:52 -0600

    Clase Bangtang

---
+++ src/Claseuno.java
@@ -0,0 +1,13 @@
+public class Claseuno {
+    int bangtang;
+    String nombreIntegrantes;
+
+    public void setBangtang(int bangtang) {
+        this.bangtang = bangtang;
+    }
+
+    public int getBangtang() {
+        return bangtang;
+    }
+
+}
